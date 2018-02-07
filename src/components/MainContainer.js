fd636bb2d77f5f6bff139e45d5adc9bfd273b6fb
roy ledford <bluesixty@gmail.com>
2018-02-07 15:05:45 -0500

    sort the list of eats

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -12,8 +12,14 @@ export default class MainContainer extends Component {
 
   componentWillMount() {
     DataService.getEats().then(eats => {
-      // const result = atob(eats.content)
-      this.setState({ eats: eats })
+      const sortedEats = eats.sort((a, b) => {
+        if (a.name > b.name) {
+          return 1
+        } else {
+          return -1
+        }
+      })
+      this.setState({ eats: sortedEats })
     })
   }
 
