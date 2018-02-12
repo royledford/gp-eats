de1deaccad62da4b8fe9502d31c7600547119f81
roy ledford <bluesixty@gmail.com>
2018-02-12 17:27:46 -0500

    change the lng of gp to make it closer to the road:

--- src/config/settings.js
+++ src/config/settings.js
@@ -2,7 +2,7 @@ module.exports = {
   homeAddress: '525 NW 1st Ave, Fort Lauderdale, FL 33301',
   zoomLevel: 14,
   homeLat: 26.1288044,
-  homeLng: -80.1449954,
+  homeLng: -80.14475,
   markerSize: {
     defaultMarkerSize: 22,
     selectedMarkerSize: 32,
